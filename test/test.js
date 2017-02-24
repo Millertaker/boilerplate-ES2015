882b0dc4acd29e0c283768f0c325c5e5ef8cdb34
Miller Gonzalez <miller.gonzalez@salesforce.com>
2017-02-23 20:45:14 -0500

    Firts basic test

---
+++ test/test.js
@@ -0,0 +1,8 @@
+let expect = require('chai').expect;
+let assert = require('chai').assert;
+
+describe('this is my firts test', function(){
+  it('shoud pass', function(){
+    assert.equal(1,1);
+  });
+})
