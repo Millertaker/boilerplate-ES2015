bf4a3a5dce896729b083e0ff542f4e483866617c
Miller Gonzalez <miller.gonzalez@salesforce.com>
2017-03-07 17:02:50 -0500

    Gulfiles ordered

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,125 +1,14 @@
-
-//////////////////////////////////////////////////////////////////////////////////////////////
-// Required files
-//////////////////////////////////////////////////////////////////////////////////////////////
-
+'use strict'
 
 import gulp from 'gulp';
-import shell from 'gulp-shell';
-import rimraf from 'rimraf';
-import run from 'run-sequence';
-import watch from 'gulp-watch';
-import server from 'gulp-live-server';
 
-import uglify from 'gulp-uglify';
-import babel from 'gulp-babel';
-import rename from 'gulp-rename';
-import less from 'gulp-less';
-import path from 'path';
-import concat from 'gulp-concat';
-import uglifycss from 'gulp-uglifycss';
-import plumber from 'gulp-plumber';
-import LessPluginAutoPrefix from 'less-plugin-autoprefix';
-import gulpif from 'gulp-if';
-
-import globalConfig from './config';
-
-//////////////////////////////////////////////////////////////////////////////////////////////
-// Vars setup
-//////////////////////////////////////////////////////////////////////////////////////////////
+let env = process.env.NODE_ENV || 'development';
 
 
-let autoprefix = new LessPluginAutoPrefix({browsers: ["last 2 versions"]});
-let express;
-
-//////////////////////////////////////////////////////////////////////////////////////////////
-// BE task
-//////////////////////////////////////////////////////////////////////////////////////////////
+require('require-dir')('./gulp')
+console.log('Invoking gulp -', env)
 
 
-gulp.task('server', () => {
-  express = server.new('./app');
-  express.start.bind(express);
-});
-
-gulp.task('build', cb => {
-  run('clean', 'babel', 'restart', cb);
-});
-
-gulp.task('clean', cb => {
-  rimraf('./app', cb);
-});
-
-gulp.task('babel', shell.task([
-    'babel src --out-dir app'
-  ])
-);
-
-gulp.task('restart', () => {
-  express.start.bind(express)();
-});
-
-
-gulp.task('watch-be', () => {
-  gulp.watch('./src/**/*.js', () => {
-    gulp.start('build');
-  });
+gulp.task('default', ['clean'], function (defaultTasks) {
+  gulp.start(env)
 })
-
-
-//////////////////////////////////////////////////////////////////////////////////////////////
-// FE task
-//////////////////////////////////////////////////////////////////////////////////////////////
-
-gulp.task('scripts', cb => {
-  rimraf('./public/js', cb);
-
-  gulp.src(['./public/src/**/*.js', '!app/js/**/*.min.js'])
-    .pipe(concat('concat.min.js'))
-    .pipe(babel(  { presets: ['es2015'] } ))
-    .pipe(gulpif(globalConfig.production(),uglify()))
-    .pipe(gulp.dest('./public/js'));
-});
-
-gulp.task('less', () => {
-  gulp.src('./public/less/**/*.less')
-
-    .pipe(less({
-      plugins: [autoprefix]
-    }))
-
-    .pipe(concat('allmin.css'))
-
-    //Minify all less
-    .pipe(
-      gulpif(globalConfig.production(),
-        uglifycss({
-          "maxLineLen": 80,
-          "uglyComments": true
-        })
-      )
-    )
-
-    .pipe(gulp.dest('./public/css'));
-});
-
-gulp.task('watch-fe', () =>{
-  gulp.watch('./public/src/**/*.js', () => {
-    gulp.start('scripts');
-  });
-
-  gulp.watch('./public/less/**/*.less', () => {
-    gulp.start('less');
-  });
-});
-
-
-//////////////////////////////////////////////////////////////////////////////////////////////
-// Commands
-//////////////////////////////////////////////////////////////////////////////////////////////
-
-
-gulp.task('default', cb => {
-  run('server', 'build', 'watch-be', 'watch-fe', 'scripts', 'less', cb);
-});
-
