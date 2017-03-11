c1528dc950781b1d983fb5814fb0e678999ad2bd
Miller Gonzalez <miller.gonzalez@salesforce.com>
2017-03-10 20:48:06 -0500

    Setup Basig Libraries

---
+++ public/assets/config.js
@@ -0,0 +1,9 @@
+//config here
+
+requirejs.config({
+  baseUrl: '/assets/',
+  paths: {
+    jquery: 'vendor/jquery/dist/jquery.min',
+    bootstrap: 'vendor/bootstrap/dist/js/bootstrap.min'
+  }
+});
