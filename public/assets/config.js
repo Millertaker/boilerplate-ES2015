6c38847005ca72c0c087eaea6f37c16ea2669918
Miller Gonzalez <miller.gonzalez@salesforce.com>
2017-03-10 22:29:40 -0500

    Implement load bootstrap and jquery

--- public/assets/config.js
+++ public/assets/config.js
@@ -4,6 +4,14 @@ requirejs.config({
   baseUrl: '/assets/',
   paths: {
     jquery: 'vendor/jquery/dist/jquery.min',
-    bootstrap: 'vendor/bootstrap/dist/js/bootstrap.min'
+    bootstrap: 'vendor/bootstrap/dist/js/bootstrap.min',
+    app: 'app/concat.min'
+  },
+
+  shim : {
+    bootstrap : {
+      deps : [ 'jquery'],
+      exports: 'Bootstrap'
+    },
   }
 });
