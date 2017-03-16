19dba6aa4a9c31a7af23e1253792cfd9d5116cd2
Miller Gonzalez <miller.gonzalez@salesforce.com>
2017-03-16 17:38:16 -0500

    Install bootstrap

--- public/src/app.js
+++ public/src/app.js
@@ -1,5 +1,9 @@
 /* Comppments Pulled */
 import linkComponent from './components/linkComponent';
+import $ from '../assets/vendor/jquery/dist/jquery.min';
+
+// Bootstrap js is stuck in 2009 so it wants globals
+global.jQuery = $
 
 let app = (linkComponent) => {
   const greeting = 'whats up vato!';
